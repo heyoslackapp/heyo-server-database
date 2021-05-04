c67483269f34012eae0e13c00cd5ac5b68aff3f6
heyoslackapp <83660841+heyoslackapp@users.noreply.github.com>
2021-05-04 13:45:07 -0500

    reset database

--- config/config.js
+++ config/config.js
@@ -2,11 +2,12 @@ process.env.PORT = process.env.PORT || 5000;
 process.env.NODE_ENV = process.env.NODE_ENV || "env";
 
 let urlDB =
-  "mongodb+srv://root:s98tiMpvCMki!T9@cluster0.u72ao.mongodb.net/heyo?retryWrites=true&w=majority";
+  "mongodb+srv://root:s98tiMpvCMki!T9@heyo.ls0xd.mongodb.net/heyo?retryWrites=true&w=majority";
 
 if (process.env.NODE_ENV === "dev") {
   urlDB =
-    "mongodb+srv://root:s98tiMpvCMki!T9@cluster0.u72ao.mongodb.net/heyo?retryWrites=true&w=majority";
+    // "mongodb+srv://root:s98tiMpvCMki!T9@cluster0.u72ao.mongodb.net/heyo?retryWrites=true&w=majority";
+    "mongodb+srv://root:s98tiMpvCMki!T9@heyo.ls0xd.mongodb.net/heyo?retryWrites=true&w=majority";
 }
 
 process.env.urlDB = urlDB;
